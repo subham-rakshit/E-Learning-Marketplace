153d898f773f604c3e7f533c1d5c09726e575328
subham-rakshit <subhamrakshit667@gmail.com>
2024-12-21 22:44:57 +0530

    Changes bg color in user pages

--- src/app/(pages)/(protectedPages)/user/instructor/course/create/page.jsx
+++ src/app/(pages)/(protectedPages)/user/instructor/course/create/page.jsx
@@ -4,8 +4,8 @@ const CreateCourses = () => {
   return (
     <>
       <div className='w-full'>
-        <div className='flex min-h-[100px] items-center justify-center bg-slate-300 p-3 sm:p-5'>
-          <h1 className='font-poppins-md text-4xl text-slate-700'>
+        <div className='flex min-h-[100px] items-center justify-center bg-banner p-3 sm:p-5'>
+          <h1 className='font-poppins-md text-4xl text-slate-200'>
             Create Courses
           </h1>
         </div>
--- src/app/(pages)/(protectedPages)/user/profile/page.jsx
+++ src/app/(pages)/(protectedPages)/user/profile/page.jsx
@@ -5,8 +5,8 @@ const UserProfile = () => {
   return (
     <>
       <div className='w-full'>
-        <div className='flex min-h-[100px] items-center justify-center bg-slate-300 p-3 sm:p-5'>
-          <h1 className='font-poppins-md text-4xl text-slate-700'>
+        <div className='flex min-h-[100px] items-center justify-center bg-banner p-3 sm:p-5'>
+          <h1 className='font-poppins-md text-4xl text-slate-200'>
             <UserDetails />
           </h1>
         </div>
